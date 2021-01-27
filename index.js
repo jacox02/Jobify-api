e3371b5696390ee2e15648d4fb503fec87fb18f6
jacox02 <anonimo4671@hotmail.com>
2021-01-27 09:27:45 +0800

    Code base API

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 const PORT = process.env.SERVER_PORT || 3050;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port${PORT}`);
 });
