c433b72c19980db793bd63c5eebd8cb95fdefb08
jacox02 <anonimo4671@hotmail.com>
2021-01-28 22:10:01 +0800

    .env file deleted

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.SERVER_PORT || 3050;
+const PORT = process.env.PORT || 3050;
 
 app.listen(PORT, () => {
   console.log(`Server running on port${PORT}`);
