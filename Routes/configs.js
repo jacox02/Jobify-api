23b7de85a2d711c0d83fee517b03ede90f13d0ab
Luis <Luistejeda0011@gmail.com>
2021-02-26 16:58:37 -0400

    Adding configs routes

---
+++ Routes/configs.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const pool = require("../Database/database");
+
+app.get("/WorkQuantity", (req, res) => {
+    pool.query(
+      `select Work_Quantity from configs where id = 1`,
+      (err, results) => {
+        if (err) throw err;
+        res.send(results);
+      }
+    );
+  });
+  app.put("/WorkQuantity/edit"), (req, res) => {
+    const quantity = req.body;
+    const setquantity = {
+      Work_Quantity: quantity
+    }
+  pool.query(
+    `UPDATE configs SET Work_Quantity = ? WHERE id = 1`, [setquantity] )
+  }
+  module.exports = app;
