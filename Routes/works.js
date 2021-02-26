e4fe13134d6fd34180a509886a40aad4b55ee964
RowinChalas <rowin077@gmail.com>
2021-02-26 17:19:45 -0400

    Update works.js

--- Routes/works.js
+++ Routes/works.js
@@ -130,4 +130,71 @@ app.get("/Works/Categories", (req, res) => {
   });
 });
 
+
+
+app.put("/Update/Works/:id", (req, res) => {
+
+  let email = req.body.Email
+  let owner_email = req.body.Owner_Email
+  let title = req.body.Work_Title
+  let location = req.body.Location
+  let position = req.body.Position
+  let description = req.body.Description
+  let aply_method = req.body.Apply_Method
+  let work_type = req.body.WorkType
+
+  pool.query(`update works 
+              set Work_Title = '${title}',
+                  Location = '${location}',
+                  Position = '${position}',
+                  Email = '${email}',
+                  Owner_Email = '${owner_email}',
+                  Description = '${description}',
+                  Apply_Method = '${aply_method}',
+                  WorkType = '${work_type}'
+              
+              where Work_ID=${req.params.id}`
+              
+              , (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+
+app.delete("/delete/Works/:id", (req,res)=>{
+  pool.query(`delete from works where Work_ID = ${req.params.id}`,
+  (err,results)=>{
+    if(err) throw err
+    res.send(results)
+  }
+  
+  )
+})
+
+
+app.put("/Update/Categories/:id", (req, res) => {
+
+  let nameCategories = req.body.Category_Name 
+  pool.query(`update categories 
+  set Category_Name = '${nameCategories}'
+  where Category_ID =${req.params.id}`
+              
+              , (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+
+app.delete("/delete/Categories/:id", (req,res)=>{
+  pool.query(`delete from categories where Category_ID = ${req.params.id}`,
+  (err,results)=>{
+    if(err) throw err
+    res.send(results)
+  }
+  
+  )
+})
+
 module.exports = app;
